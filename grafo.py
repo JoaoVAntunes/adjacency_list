4b88f1bd77a84f06f15c39d63a13bc43a9235f89
JoaoVAntunes <joaovitorantunes15@hotmail.com>
2025-03-19 18:10:14 -0300

    out_degree and in_degree functions added

--- grafo.py
+++ grafo.py
@@ -2,45 +2,80 @@ from collections import defaultdict
 
 class Graph:
     def __init__(self):
-        self.adj_list = defaultdict(list)
-        self.order = 0
-        self.size = 0
+        self.adj_list = defaultdict(list) 
+        self.order = 0       
+        self.size = 0   
 
-    def add_node(self, u):
+    def add_node(self, u: str):
         if u in self.adj_list:
-            print("Node already exists")
+            print(f"Node '{u}' already exists.")
             return
         else:
-            self.adj_list = u
+            self.adj_list[u] = []  
             self.order += 1
-            print("Node addded succesfully ")
+            print(f"Node '{u}' added successfully.")
 
-    def add_edge(self, u, v, weight):  
+    def add_edge(self, u: str, v: str, weight: int):  
         if weight < 0:
-            print("Invalid weight")
+            print("Invalid weight.")
             return
 
-        elif u not in self.adj_list:
+        if u not in self.adj_list:
             self.add_node(u)
 
-        elif v not in self.adj_list:
+        if v not in self.adj_list:
             self.add_node(v)
 
-        # Verifica se a aresta já existe e atualiza o peso
+        # Verifies an existing edge, if already exists updates the wieght
         for i, (vertice, p) in enumerate(self.adj_list[u]):
             if vertice == v:
-                self.adj_list[u][i] = (v, weight)
-                print("Edge updated successfully!")
+                self.adj_list[u][i] = (v, weight)  
+                print(f"Edge between '{u}' and '{v}' updated successfully!")
                 return
         
         self.adj_list[u].append((v, weight))
         self.size += 1
-        print("Edge added succesfully!")
-    
-    def edge_between(self, u, v):
-        #List Comprehension: "for node, weight in self..." percorre this tuple list trting to find a node equals "v"
-        return any( node == v for node, weight in self.adj_list[u])
-    
-    def there_is_edge(self, u):
-          return bool(self.adj_list[u])
+        print(f"Edge between '{u}' and '{v}' added successfully!")
 
+    #Verifies if the nodes have and adge in between
+    def edge_between(self, u: str, v: str):
+        #List Comprehension: "for node, weight in self..." percorre this tuple list trying to find a node equals "v"
+        return any(node == v for node, weight in self.adj_list[u])
+
+    #Verifies if the node has any edges
+    def there_is_edge(self, u: str):
+        return bool(self.adj_list[u])
+
+    #Checks how many edges point to a node 
+    def in_degree(self, u):
+        if u not in self.adj_list:
+            print(f"Node '{u}' does not exists.")
+            return
+        
+        else:
+            degree = 0
+            for node, edges in self.adj_list.items():
+                for v, w in edges:
+                    if v == u:
+                        degree += 1
+            print("The amount of edges pointing to this node is: ", degree)
+
+    def out_degree(self, u):
+        if u not in self.adj_list:
+            print(f"Node '{u}' does not exists.")
+            return
+        
+        else:
+            degree = 0
+            for i in enumerate(self.adj_list[u]):
+                degree += 1
+            print(f"This node has: {degree} edge(s)")
+
+
+    def toString(self):
+        print("\nGraph representation:\n")
+        for node, edges in self.adj_list.items():
+            if edges:
+                print(f"Node {node}: {''.join([f'({v}, {w}) -> ' for v, w in edges])}")
+            else:
+                print(f"Node {node}: No edges.")
--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-import adjacency_list.grafo as grafo
+import grafo as graph
 
+G = graph.Graph()
+G.add_node("Joao")
+G.add_node("Maria")
+G.add_edge("Joao", "Maria", 3)
+G.add_edge("Joao", "Caio", 60)
+G.add_edge("Maria", "Clara", 99)
+
+
+# print("\n", G.edge_between("Joao", "Maria"))
+# print(G.edge_between("Joao", "Clara"))
+
+# print("\n", G.there_is_edge("Joao"))
+# print(G.there_is_edge("Caio"))
+
+#G.in_degree("Maria")
+G.out_degree("Joao")
+
+
+G.toString()
